5664440c5c71466bbf0d6a343f7b46897e64bd84
SeanDiRienzo <seandirienzo@gmail.com>
2019-11-21 17:02:25 -0500

    added toolbar to each activity added help strings into xml files for translation added favourites activity list

---
+++ app/src/main/java/com/example/finalproject/news/DetailContainer.java
@@ -0,0 +1,13 @@
+package com.example.finalproject.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
