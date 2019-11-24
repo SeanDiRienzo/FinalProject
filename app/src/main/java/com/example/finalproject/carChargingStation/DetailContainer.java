45eba6a12aa8dc707ae4fb4801bfbcbd9b271d2e
Svitlana <svitlana.tsushka91@gmail.com>
2019-11-23 23:29:03 -0500

    Finished a fragment for carcharging module

---
+++ app/src/main/java/com/example/finalproject/carChargingStation/DetailContainer.java
@@ -0,0 +1,32 @@
+package com.example.finalproject.carChargingStation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finalproject.R;
+import com.example.finalproject.news.DetailFragment;
+import com.example.finalproject.news.NewsArticleObject;
+
+public class DetailContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_details_fragment);
+
+
+        Bundle bundle = getIntent().getExtras();
+
+
+        StationFragment dFragment = new StationFragment();
+        dFragment.setArguments(bundle); //pass data to the the fragment
+        dFragment.setTablet(false); //tell the Fragment that it's on a phone.
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentLocation, dFragment)
+
+                .commit();
+    }
+}
